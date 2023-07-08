8eed471f45226b6c323db278022d15fd935092dc
henrikrxn <20686751+henrikrxn@users.noreply.github.com>
2023-07-08 14:09:33 +0200

    First example of a dotnet program loading a wasm file and executing it

---
+++ dotnet-calling-wat/Program.cs
@@ -0,0 +1,17 @@
+﻿using Wasmtime;
+
+using var engine = new Engine();
+using var module = Module.FromTextFile(engine, "add.wat");
+using var store = new Store(engine);
+
+var instance = new Instance(store, module, Array.Empty<object>());
+
+var add = instance.GetFunction<int, int, int>("add");
+
+if (add is null)
+{
+    Console.WriteLine("Failed to load function add");
+    Environment.Exit(1);
+}
+
+Console.WriteLine($"add(27, 6) = {add(27, 6)}");
