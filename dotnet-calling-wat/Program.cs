38acf66510203aaff3426cb0efcf8c99aed1233d
henrikrxn <20686751+henrikrxn@users.noreply.github.com>
2023-07-27 20:35:30 +0200

    Using calculator.wat instead of the local add.wat

--- dotnet-calling-wat/Program.cs
+++ dotnet-calling-wat/Program.cs
@@ -1,7 +1,7 @@
 ﻿using Wasmtime;
 
 using var engine = new Engine();
-using var module = Module.FromTextFile(engine, "add.wat");
+using var module = Module.FromTextFile(engine, @"..\samples\wasm\wat\calculator\calculator.wat");
 using var store = new Store(engine);
 
 var instance = new Instance(store, module, Array.Empty<object>());
