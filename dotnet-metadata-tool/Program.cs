ddb0067de9c0bc8f833316485d5fedeaecdb4597
henrikrxn <20686751+henrikrxn@users.noreply.github.com>
2023-07-29 18:18:09 +0200

    WIP Adding project for wasm-metadata-tool

---
+++ dotnet-metadata-tool/Program.cs
@@ -0,0 +1,7 @@
+﻿using Wasmtime;
+
+using var engine = new Engine();
+using var module = Module.FromTextFile(engine, "add.wat");
+using var store = new Store(engine);
+
+var instance = new Instance(store, module, Array.Empty<object>());
