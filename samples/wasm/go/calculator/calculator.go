36472a1e08bc52f96e99baf4d50ea4f40a719b3b
henrikrxn <20686751+henrikrxn@users.noreply.github.com>
2023-08-26 15:53:17 +0200

    Removed various comments about stuff in tinygo

--- samples/wasm/go/calculator/calculator.go
+++ samples/wasm/go/calculator/calculator.go
@@ -11,23 +11,15 @@ package main
 //	return 0
 //}
 
-// DOES NOT WORK IN tinygo 0.28.1:  go:wasm-module calculator-in-go
-// See https://github.com/tinygo-org/tinygo/issues/3839
-//
-//export add
+//go:export add
 func add(x int32, y int32) int32 {
 	return x + y
 }
 
-// DOES NOT WORK IN tinygo 0.28.1:  go:wasm-module calculator-in-go
-// See https://github.com/tinygo-org/tinygo/issues/3839
-//
-//export subtract
+//go:export subtract
 func subtract(x int32, y int32) int32 {
 	return x - y
 }
 
 // Needed even though it is a noop
 func main() {}
-
-// Right now it seems like tinygo does not care and always builds wasi ?
