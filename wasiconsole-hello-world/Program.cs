c3149dba3383ada982b60d8d16b89a86f33a6c9b
henrikrxn <20686751+henrikrxn@users.noreply.github.com>
2024-03-28 10:24:15 +0100

    Updating WASI console program to display more runtime information

--- wasiconsole-hello-world/Program.cs
+++ wasiconsole-hello-world/Program.cs
@@ -1,4 +1,7 @@
 using System;
 using System.Runtime.InteropServices;
 
-Console.WriteLine($"Hello, This is Wasi Console on {RuntimeInformation.OSArchitecture}!");
+Console.WriteLine($"OS Description       : {RuntimeInformation.OSDescription}");
+Console.WriteLine($"OS Architecture      : {RuntimeInformation.OSArchitecture}");
+Console.WriteLine($"Framework Description: {RuntimeInformation.FrameworkDescription}");
+Console.WriteLine($"Runtime Identifier   : {RuntimeInformation.RuntimeIdentifier}");
