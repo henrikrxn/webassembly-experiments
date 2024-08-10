f95234732abf03a170cc5a5f34479a77e0223c80
henrikrxn <20686751+henrikrxn@users.noreply.github.com>
2024-08-10 11:57:22 +0200

    First version
    * Builds, but needs refinement, e.g. to get a single wasm file

---
+++ dotnet-9/wasi-library/Calculator.cs
@@ -0,0 +1,14 @@
+﻿namespace wasi_library;
+
+public class Calculator
+{
+    public int add(int x, int y)
+    {
+        return x + y;
+    }
+
+    public int subtract(int x, int y)
+    {
+        return x - y;
+    }
+}
