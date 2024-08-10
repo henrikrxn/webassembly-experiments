1753156e9c37f99cf4ff17a578fa2db9bfb4a365
henrikrxn <20686751+henrikrxn@users.noreply.github.com>
2024-08-10 21:15:59 +0200

    Updates to allow for export

--- dotnet-9/wasi-library/Calculator.cs
+++ dotnet-9/wasi-library/Calculator.cs
@@ -1,13 +1,17 @@
-﻿namespace wasi_library;
+﻿using System.Runtime.InteropServices;
+
+namespace wasi_library;
 
 public class Calculator
 {
-    public int add(int x, int y)
+    [UnmanagedCallersOnly(EntryPoint = "add")]
+    public static int Add(int x, int y)
     {
         return x + y;
     }
 
-    public int subtract(int x, int y)
+    [UnmanagedCallersOnly(EntryPoint = "subtract")]
+    public static int Subtract(int x, int y)
     {
         return x - y;
     }
