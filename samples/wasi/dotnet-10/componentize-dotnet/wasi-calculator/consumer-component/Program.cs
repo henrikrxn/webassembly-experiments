2370897cf6385d29cf516d3e0bea34ce6bde87e6
henrikrxn <20686751+henrikrxn@users.noreply.github.com>
2025-07-13 15:29:23 +0200

    Adding more projects
    * library consumer that calls the calculator library
    * composed that combines the other two components

---
+++ samples/wasi/dotnet-10/componentize-dotnet/wasi-calculator/consumer-component/Program.cs
@@ -0,0 +1,9 @@
+﻿using HostappWorld.wit.imports.example.calculator;
+
+var x = 456;
+var y = 123;
+var added = OperationsInterop.Add(x, y);
+Console.WriteLine($"{x} + {y} = {added}");
+
+var subtracted = OperationsInterop.Subtract(x, y);
+Console.WriteLine($"{x} - {y} = {subtracted}");
