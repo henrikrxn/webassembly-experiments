b2bed8f0156bada3ec1915f47d046207e45b1abd
henrikrxn <20686751+henrikrxn@users.noreply.github.com>
2025-07-13 19:11:34 +0200

    Adding code from wasiconsole-hello-world

--- samples/wasi/dotnet-10/componentize-dotnet/componentize-wasi-cli/Program.cs
+++ samples/wasi/dotnet-10/componentize-dotnet/componentize-wasi-cli/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+using System.Runtime.InteropServices;
+
 Console.WriteLine("Hello world from compontize-dotnet!");
+
+Console.WriteLine($"OS Description       : {RuntimeInformation.OSDescription}");
+Console.WriteLine($"OS Architecture      : {RuntimeInformation.OSArchitecture}");
+Console.WriteLine($"Framework Description: {RuntimeInformation.FrameworkDescription}");
+Console.WriteLine($"Runtime Identifier   : {RuntimeInformation.RuntimeIdentifier}");
