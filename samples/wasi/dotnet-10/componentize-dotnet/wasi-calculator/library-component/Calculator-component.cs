f2148c497d42e623388b95fa2ac3cd4600529a89
henrikrxn <20686751+henrikrxn@users.noreply.github.com>
2025-07-13 13:07:39 +0200

    Adding library implementing add and subtract

---
+++ samples/wasi/dotnet-10/componentize-dotnet/wasi-calculator/library-component/Calculator-component.cs
@@ -0,0 +1,14 @@
+﻿namespace ComputerWorld.wit.exports.example.calculator;
+
+public class OperationsImpl : IOperations
+{
+    public static int Add(int x, int y)
+    {
+        return x + y;
+    }
+
+    public static int Subtract(int x, int y)
+    {
+        return x - y;
+    }
+}
