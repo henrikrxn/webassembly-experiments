78f30ee316affc5f2409d9dcb035743e9bc12e07
henrikrxn <20686751+henrikrxn@users.noreply.github.com>
2025-08-19 20:57:50 +0200

    Updating dotnet-10 WASI console
    * Additional code will expose if it really is a WASI program

--- samples/dotnet-10/wasiconsole-dotnet-10-out-of-the-box/Program.cs
+++ samples/dotnet-10/wasiconsole-dotnet-10-out-of-the-box/Program.cs
@@ -1,3 +1,9 @@
 using System;
+using System.Runtime.InteropServices;
 
 Console.WriteLine("Hello, Wasi Console!");
+
+Console.WriteLine($"OS Description       : {RuntimeInformation.OSDescription}");
+Console.WriteLine($"OS Architecture      : {RuntimeInformation.OSArchitecture}");
+Console.WriteLine($"Framework Description: {RuntimeInformation.FrameworkDescription}");
+Console.WriteLine($"Runtime Identifier   : {RuntimeInformation.RuntimeIdentifier}");
--- samples/dotnet-10/wasmtime-dotnet/dotnet-10-calling-wasm-wat/README.md
+++ samples/dotnet-10/wasmtime-dotnet/dotnet-10-calling-wasm-wat/README.md
@@ -5,3 +5,12 @@ As simple as it gets:
 ```Powershell
 > dotnet run -c Debug
 ```
+
+doesn't work as reported in this [Github issue](https://github.com/dotnet/runtime/issues/117848), but in a comment
+on the same issue a contributor revealed how to make it work:
+
+```Powershell
+> dotnet build -c Debug
+> cd bin\Debug\net10.0\wasi-wasm\AppBundle
+> wasmtime run --wasi http --dir . dotnet.wasm wasiconsole-dotnet-10
+```
